ea10e3a2dec135e57b43d48d281361e414857cb3
dv1sual <53606943+dv1sual@users.noreply.github.com>
2023-02-15 22:42:51 +0000

    Prompt the user to select a .csv file of choice.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from PyQt6.QtWidgets import QApplication, QFileDialog
+import os
 
 # Create a Qt application
 app = QApplication([])
@@ -14,30 +15,26 @@ file_dialog.exec()
 file_paths = file_dialog.selectedFiles()
 if file_paths:
     file_path = file_paths[0]
+    file_name = os.path.basename(file_path)
+else:
+    file_name = ""
 
 # Read in the CSV data using pandas
 data = pd.read_csv(file_path)
 
-# Extract the data for each column
-x_data = data['Head 0 FPS']
-#  y_data = data['Head 1 FPS'] #  might be useful for 4 out servers
-#  z_data = data['Head 2 FPS']
-#  w_data = data['Head 3 FPS']
+# Extract the column names and data from the DataFrame
+column_names = list(data.columns)
 
-# Create a line chart using matplotlib
-plt.plot(x_data)
-
-# Add grid lines to the chart
-plt.grid(True)
-
-# Create a line chart using matplotlib
-plt.plot(x_data, color='red', linestyle='-')
-
-
-# Set the title and axis labels
-plt.title("FPS Monitor")
-plt.xlabel("Head 0")
-plt.ylabel("FPS")
+# Plot each column in a separate subplot
+num_columns = len(column_names)
+fig, axs = plt.subplots(num_columns, 1, figsize=(8, num_columns*3), sharex=True)
+fig.suptitle(f'{file_name}', fontsize=20, fontweight='bold')
+fig.subplots_adjust(top=0.9)
+for i in range(num_columns):
+    column_name = column_names[i]
+    x_data = data[column_name]
+    axs[i].plot(x_data)
+    axs[i].set_ylabel(column_name)
 
 # Display the chart
 plt.show()
